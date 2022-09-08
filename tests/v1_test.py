cfbd1a95a8f4630a7eacb14ba63c13102eaf370a
Víctor Roldán Betancort <vroldanbet@authzed.com>
2022-09-08 11:29:28 +0100

    add lookupresources and lookupsubject tests

--- tests/v1_test.py
+++ tests/v1_test.py
@@ -7,6 +7,8 @@ from authzed.api.v1 import (
     CheckPermissionResponse,
     Client,
     Consistency,
+    LookupResourcesRequest,
+    LookupSubjectsRequest,
     ObjectReference,
     ReadSchemaRequest,
     Relationship,
@@ -30,73 +32,23 @@ def client(token):
 
 
 def test_basic_schema(client):
-    SCHEMA = """
+    schema = """
         definition document {
             relation reader: user
         }
         definition user {}
     """
-    client.WriteSchema(WriteSchemaRequest(schema=SCHEMA))
+    client.WriteSchema(WriteSchemaRequest(schema=schema))
 
     resp = client.ReadSchema(ReadSchemaRequest())
     assert "definition document" in resp.schema_text
     assert "definition user" in resp.schema_text
 
 
-def test_basic_operations(client):
+def test_check(client):
     # Write a basic schema.
-    SCHEMA = """
-        definition post {
-            relation writer: user
-            relation reader: user
-
-            permission write = writer
-            permission view = reader + writer
-        }
-        definition user {}
-    """
-    client.WriteSchema(WriteSchemaRequest(schema=SCHEMA))
-
-    emilia = SubjectReference(
-        object=ObjectReference(
-            object_type="user",
-            object_id="emilia",
-        )
-    )
-    beatrice = SubjectReference(
-        object=ObjectReference(
-            object_type="user",
-            object_id="beatrice",
-        )
-    )
-
-    post_one = ObjectReference(object_type="post", object_id="1")
-
-    # Add some relationships.
-    client.WriteRelationships(
-        WriteRelationshipsRequest(
-            updates=[
-                # Emilia is a Writer on Post 1
-                RelationshipUpdate(
-                    operation=RelationshipUpdate.Operation.OPERATION_CREATE,
-                    relationship=Relationship(
-                        resource=post_one,
-                        relation="writer",
-                        subject=emilia,
-                    ),
-                ),
-                # Beatrice is a Reader on Post 1
-                RelationshipUpdate(
-                    operation=RelationshipUpdate.Operation.OPERATION_CREATE,
-                    relationship=Relationship(
-                        resource=post_one,
-                        relation="reader",
-                        subject=beatrice,
-                    ),
-                ),
-            ]
-        )
-    )
+    write_test_schema(client)
+    beatrice, emilia, post_one, post_two = write_test_tuples(client)
 
     # Issue some checks.
     resp = client.CheckPermission(
@@ -138,3 +90,111 @@ def test_basic_operations(client):
         )
     )
     assert resp.permissionship == CheckPermissionResponse.PERMISSIONSHIP_NO_PERMISSION
+
+
+def test_lookup_resources(client):
+    # Write a basic schema.
+    write_test_schema(client)
+    beatrice, emilia, post_one, post_two = write_test_tuples(client)
+
+    resp = client.LookupResources(
+        LookupResourcesRequest(
+            resource_object_type="post",
+            permission="write",
+            subject=emilia,
+            consistency=Consistency(fully_consistent=True),
+        )
+    )
+    responses = []
+    for response in resp:
+        responses.append(response.resource_object_id)
+    assert len(responses) == 2
+    assert responses.count(post_one.object_id) == 1
+    assert responses.count(post_two.object_id) == 1
+
+
+def test_lookup_subjects(client):
+    # Write a basic schema.
+    write_test_schema(client)
+    beatrice, emilia, post_one, post_two = write_test_tuples(client)
+
+    resp = client.LookupSubjects(
+        LookupSubjectsRequest(
+            subject_object_type="user",
+            permission="view",
+            resource=post_one,
+            consistency=Consistency(fully_consistent=True),
+        )
+    )
+    responses = []
+    for response in resp:
+        responses.append(response.subject_object_id)
+    assert len(responses) == 2
+    assert responses.count(emilia.object.object_id) == 1
+    assert responses.count(beatrice.object.object_id) == 1
+
+
+def write_test_tuples(client):
+    emilia = SubjectReference(
+        object=ObjectReference(
+            object_type="user",
+            object_id="emilia",
+        )
+    )
+    beatrice = SubjectReference(
+        object=ObjectReference(
+            object_type="user",
+            object_id="beatrice",
+        )
+    )
+    post_one = ObjectReference(object_type="post", object_id="post-one")
+    post_two = ObjectReference(object_type="post", object_id="post-two")
+    # Add some relationships.
+    client.WriteRelationships(
+        WriteRelationshipsRequest(
+            updates=[
+                # Emilia is a Writer on Post 1
+                RelationshipUpdate(
+                    operation=RelationshipUpdate.Operation.OPERATION_CREATE,
+                    relationship=Relationship(
+                        resource=post_one,
+                        relation="writer",
+                        subject=emilia,
+                    ),
+                ),
+                # Emilia is a Writer on Post 2
+                RelationshipUpdate(
+                    operation=RelationshipUpdate.Operation.OPERATION_CREATE,
+                    relationship=Relationship(
+                        resource=post_two,
+                        relation="writer",
+                        subject=emilia,
+                    ),
+                ),
+                # Beatrice is a Reader on Post 1
+                RelationshipUpdate(
+                    operation=RelationshipUpdate.Operation.OPERATION_CREATE,
+                    relationship=Relationship(
+                        resource=post_one,
+                        relation="reader",
+                        subject=beatrice,
+                    ),
+                ),
+            ]
+        )
+    )
+    return beatrice, emilia, post_one, post_two
+
+
+def write_test_schema(client):
+    schema = """
+        definition post {
+            relation writer: user
+            relation reader: user
+
+            permission write = writer
+            permission view = reader + writer
+        }
+        definition user {}
+    """
+    client.WriteSchema(WriteSchemaRequest(schema=schema))
