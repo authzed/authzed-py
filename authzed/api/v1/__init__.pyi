7f72080339cd1a014492a05e8ff947fce96b8ce9
Anil Kulkarni <akulkarni@anaconda.com>
2024-04-18 13:37:30 -0700

    Add type hints for the Client class

--- authzed/api/v1/__init__.pyi
+++ authzed/api/v1/__init__.pyi
@@ -11,6 +11,19 @@ from authzed.api.v1.watch_service_pb2_grpc import WatchServiceStub, WatchService
 import grpc
 from typing import Optional, Sequence, Tuple, Any
 
+class Client(SchemaServiceStub, PermissionsServiceStub, ExperimentalServiceStub, WatchServiceStub):
+    """The Client is typed as a synchronous client (though in practice it works with both sync and async code).
+    If you are using the async code, you should switch to the AsyncClient class instead in order to get proper type hints
+    """
+
+    def __init__(
+        self,
+        target: str,
+        credentials: grpc.ChannelCredentials,
+        options: Optional[Sequence[Tuple[str, Any]]] = None,
+        compression: Optional[grpc.Compression] = None,
+    ) -> None: ...
+
 class SyncClient(
     SchemaServiceStub, PermissionsServiceStub, ExperimentalServiceStub, WatchServiceStub
 ):
