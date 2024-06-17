81b532e0e73a4ae3622d9b7e530096a2b004849c
Mateen Kasim <mateenkasim18@gmail.com>
2024-06-17 13:53:05 -0400

    Format with black, isort

--- authzed/api/v1/__init__.pyi
+++ authzed/api/v1/__init__.pyi
@@ -1,4 +1,4 @@
-from typing import Optional, Sequence, Tuple, Any
+from typing import Any, Optional, Sequence, Tuple
 
 import grpc
 
@@ -25,7 +25,10 @@ from authzed.api.v1.experimental_service_pb2 import (
     BulkImportRelationshipsRequest,
     BulkImportRelationshipsResponse,
 )
-from authzed.api.v1.experimental_service_pb2_grpc import ExperimentalServiceStub, ExperimentalServiceAsyncStub
+from authzed.api.v1.experimental_service_pb2_grpc import (
+    ExperimentalServiceAsyncStub,
+    ExperimentalServiceStub,
+)
 from authzed.api.v1.permission_service_pb2 import (
     CheckBulkPermissionsPair,
     CheckBulkPermissionsRequest,
@@ -51,16 +54,19 @@ from authzed.api.v1.permission_service_pb2 import (
     WriteRelationshipsRequest,
     WriteRelationshipsResponse,
 )
-from authzed.api.v1.permission_service_pb2_grpc import PermissionsServiceStub, PermissionsServiceAsyncStub
+from authzed.api.v1.permission_service_pb2_grpc import (
+    PermissionsServiceAsyncStub,
+    PermissionsServiceStub,
+)
 from authzed.api.v1.schema_service_pb2 import (
     ReadSchemaRequest,
     ReadSchemaResponse,
     WriteSchemaRequest,
     WriteSchemaResponse,
 )
-from authzed.api.v1.schema_service_pb2_grpc import SchemaServiceStub, SchemaServiceAsyncStub
+from authzed.api.v1.schema_service_pb2_grpc import SchemaServiceAsyncStub, SchemaServiceStub
 from authzed.api.v1.watch_service_pb2 import WatchRequest, WatchResponse
-from authzed.api.v1.watch_service_pb2_grpc import WatchServiceStub, WatchServiceAsyncStub
+from authzed.api.v1.watch_service_pb2_grpc import WatchServiceAsyncStub, WatchServiceStub
 
 class Client(SchemaServiceStub, PermissionsServiceStub, ExperimentalServiceStub, WatchServiceStub):
     """The Client is typed as a synchronous client (though in practice it works with both sync and async code).
