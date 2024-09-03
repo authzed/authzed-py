3436a9efbbca7f9744d6bfdfe292f6502667a3d5
Tanner Stirrat <tstirrat@gmail.com>
2024-09-03 16:50:11 -0600

    Add common calls file

---
+++ tests/calls.py
@@ -0,0 +1,24 @@
+from authzed.api.v1 import (
+    WriteSchemaRequest
+)
+
+from .utils import maybe_await
+
+async def write_test_schema(client):
+    schema = """
+        caveat likes_harry_potter(likes bool) {
+          likes == true
+        }
+
+        definition post {
+            relation writer: user
+            relation reader: user
+            relation caveated_reader: user with likes_harry_potter
+
+            permission write = writer
+            permission view = reader + writer
+            permission view_as_fan = caveated_reader + writer
+        }
+        definition user {}
+    """
+    await maybe_await(client.WriteSchema(WriteSchemaRequest(schema=schema)))
